e213f878204927b20a2498f48e1d66b5088c3544
grypesc <g.rypesc@wp.pl>
2020-12-09 01:07:59 +0100

    data splitting

---
+++ utils/split_data.py
@@ -0,0 +1,21 @@
+from random import shuffle
+
+
+def save_set(set, data_path):
+    with open(data_path + "_eng.txt", 'w') as file:
+        file.writelines(["%s\n" % item[0] for item in set])
+    with open(data_path + "_spa.txt", 'w') as file:
+        file.writelines(["%s\n" % item[1] for item in set])
+
+
+if __name__ == '__main__':
+    lines = open("../data/spa.txt", encoding='utf-8').read().strip().split('\n')
+    pairs = [[s for s in l.split('\t')[0:2]] for l in lines]
+    shuffle(pairs)
+    train_set = pairs[0:int(0.6 * len(pairs))]
+    validation_set = pairs[int(0.6 * len(pairs)):int(0.8 * len(pairs))]
+    test_set = pairs[int(0.8 * len(pairs)):]
+
+    save_set(train_set, "../data/train")
+    save_set(validation_set, "../data/validation")
+    save_set(test_set, "../data/test")
