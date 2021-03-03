06800cf1f2ee44551e0fcab0cd9d52cd735b5b3a
grypesc <g.rypesc@wp.pl>
2021-03-03 16:34:34 +0100

    updated bleu calculation

--- utils/tools.py
+++ utils/tools.py
@@ -19,13 +19,10 @@ def int_to_one_hot(index: int, length, device):
 
 def bleu(output, target, vocab, device):
     _, output_indices = torch.max(output, 2)
-    eos_pad = torch.full((1, output.size()[1]), vocab.stoi["<eos>"], device=device)
-    output_indices = torch.cat((output_indices, eos_pad), 0)
     output_indices = output_indices.transpose(1, 0)
     target = target.transpose(1, 0)
-
-    output_str = [[vocab.itos[x] for x in y] for y in output_indices]
-    output_str = [x[:x.index("<eos>")] for x in output_str]
     target_str = [[vocab.itos[x] for x in y] for y in target]
     target_str = [[x[:x.index("<eos>")]] for x in target_str]
+    output_str = [[vocab.itos[x] for x in y] for y in output_indices]
+    output_str = [x[:len(target_str[i][0])] for i, x in enumerate(output_str)]
     return bleu_score(output_str, target_str)
