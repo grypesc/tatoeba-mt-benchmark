39df38c48d59db277a71d08ead29a5dbfbab2816
grypesc <g.rypesc@wp.pl>
2021-03-12 21:54:27 +0100

    added more approximators

--- rql.py
+++ rql.py
@@ -8,34 +8,11 @@ import torch.optim as optim
 
 from utils.data_pipeline_rql import DataPipelineRQL
 from utils.tools import epoch_time, bleu, actions_ratio
+from utils.rql_models import RQL, RQL1, RQL2
 
 torch.set_printoptions(threshold=10_000)
 random.seed(20)
 torch.manual_seed(20)
-
-
-class RQL(nn.Module):
-    def __init__(self,
-                 input_dim: int,
-                 src_emb_dim: int,
-                 trg_emb_dim: int,
-                 rnn_hid_dim: int,
-                 num_layers: int,
-                 output_dim: int):
-        super().__init__()
-
-        self.src_embedding = nn.Embedding(input_dim, src_emb_dim).from_pretrained(en_vocab.vectors, freeze=True)
-        self.trg_embedding = nn.Embedding(input_dim, trg_emb_dim).from_pretrained(spa_vocab.vectors, freeze=True)
-        self.rnn = nn.GRU(src_emb_dim + trg_emb_dim, rnn_hid_dim, num_layers=num_layers, bidirectional=False, dropout=0)
-        self.output = nn.Linear(rnn_hid_dim, output_dim)
-
-    def forward(self, src, previous_output, rnn_state):
-        src_embedded = self.src_embedding(src)
-        trg_embedded = self.trg_embedding(previous_output)
-        rnn_input = torch.cat((src_embedded, trg_embedded), dim=2)
-        rnn_output, rnn_state = self.rnn(rnn_input, rnn_state)
-        outputs = self.output(rnn_output)
-        return outputs, rnn_state
 
 
 def episode(src, trg, epsilon, teacher_forcing):
@@ -43,13 +20,14 @@ def episode(src, trg, epsilon, teacher_forcing):
     src_seq_len = src.size()[0]
     trg_seq_len = trg.size()[0]
     word_output = torch.tensor(([batch_size * [spa_vocab.stoi["<null>"]]]), device=device)
-    rnn_state = torch.zeros((NUM_LAYERS, batch_size, RNN_HID_DIM), device=device)
+    rnn_state = torch.zeros((NUM_RNN_LAYERS, batch_size, RNN_HID_DIM), device=device)
 
     word_outputs = torch.zeros((trg_seq_len, batch_size, len(spa_vocab)), device=device)
     Q_used = torch.zeros((src_seq_len + trg_seq_len, batch_size), device=device)
     Q_target = torch.zeros((src_seq_len + trg_seq_len, batch_size), device=device)
 
     writing_agents = torch.full((batch_size,), False, device=device)
+    naughty_agents = torch.full((batch_size,), False, device=device)
     terminated_agents = torch.full((batch_size,), False, device=device)
     terminated_on = torch.full((batch_size,), -1, device=device)
 
@@ -61,6 +39,7 @@ def episode(src, trg, epsilon, teacher_forcing):
     while True:
         input = torch.gather(src, 0, i)
         input[:, writing_agents] = EN_NULL
+        input[:, naughty_agents] = EN_PAD
         output, rnn_state = model(input, word_output, rnn_state)
         _, word_output = torch.max(output[:, :, :-3], dim=2)
 
@@ -86,8 +65,7 @@ def episode(src, trg, epsilon, teacher_forcing):
 
         just_terminated_agents = agents_outputting * (torch.gather(trg, 0, j) == SPA_EOS).squeeze_()
         naughty_agents = (reading_agents + bothing_agents) * (torch.gather(src, 0, i) == EN_EOS).squeeze_()
-        just_terminated_agents = just_terminated_agents + naughty_agents
-        i = i + (reading_agents + bothing_agents)
+        i = i + ~naughty_agents * (reading_agents + bothing_agents)
         old_j = j
         j = j + agents_outputting
 
@@ -104,6 +82,7 @@ def episode(src, trg, epsilon, teacher_forcing):
         with torch.no_grad():
             _input = torch.gather(src, 0, i)
             _input[:, writing_agents] = EN_NULL
+            _input[:, naughty_agents] = EN_PAD
             _output, _ = model(_input, word_output, rnn_state)
             next_best_action_value, _ = torch.max(_output[:, :, -3:], 2)
 
@@ -112,9 +91,9 @@ def episode(src, trg, epsilon, teacher_forcing):
             Q_target[t, terminated_agents] = 0
             Q_target[t, reading_agents] = next_best_action_value[0, reading_agents]
             Q_target[t, just_terminated_agents] = reward[just_terminated_agents]
-            Q_target[t, naughty_agents] = -50.0
+            Q_target[t, naughty_agents] = Q_target[t, naughty_agents] - 10.0
 
-            if terminated_agents.all():
+            if terminated_agents.all() or t >= src_seq_len + trg_seq_len - 1:
                 return word_outputs, Q_used, Q_target, actions_count
         t += 1
 
@@ -164,8 +143,9 @@ def evaluate(loader):
 
 
 BATCH_SIZE = 64
-RNN_HID_DIM = 1024
-NUM_LAYERS = 1
+RNN_HID_DIM = 512
+DROPOUT = 0.0
+NUM_RNN_LAYERS = 1
 DISCOUNT = 0.99
 MISTRANSLATION_LOSS_MULTIPLIER = 100
 epsilon = 0.5
@@ -180,7 +160,7 @@ valid_loader = data.valid_loader
 test_loader = data.test_loader
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = RQL(len(en_vocab), en_vocab.vectors.size()[1], spa_vocab.vectors.size()[1], RNN_HID_DIM, NUM_LAYERS, len(spa_vocab) + 3).to(device)
+model = RQL(en_vocab, spa_vocab, RNN_HID_DIM, DROPOUT, NUM_RNN_LAYERS).to(device)
 
 optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
 lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.999, last_epoch=-1)
@@ -191,10 +171,12 @@ policy_loss = nn.MSELoss()
 
 SPA_NULL = torch.tensor([spa_vocab.stoi['<null>']]).to(device)
 EN_NULL = torch.tensor([en_vocab.stoi['<null>']]).to(device)
+EN_PAD = torch.tensor([en_vocab.stoi['<pad>']]).to(device)
 SPA_EOS = torch.tensor([spa_vocab.stoi['<eos>']]).to(device)
 EN_EOS = torch.tensor([en_vocab.stoi['<eos>']]).to(device)
 
 N_EPOCHS = 30
+
 print(f'The model has {sum(p.numel() for p in model.parameters() if p.requires_grad):,} trainable parameters')
 # profile = cProfile.Profile()
 # profile.enable()
---
+++ utils/rql_models.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+
+
+class RQL(nn.Module):
+    def __init__(self,
+                 src_vocab,
+                 trg_vocab,
+                 rnn_hid_dim,
+                 dropout,
+                 num_rnn_layers):
+        super().__init__()
+
+        src_emb_dim = src_vocab.vectors.size()[1]
+        trg_emb_dim = trg_vocab.vectors.size()[1]
+        self.src_embedding = nn.Embedding(len(src_vocab), src_emb_dim).from_pretrained(src_vocab.vectors, freeze=True)
+        self.trg_embedding = nn.Embedding(len(trg_vocab), trg_emb_dim).from_pretrained(trg_vocab.vectors, freeze=True)
+        self.rnn = nn.GRU(src_emb_dim + trg_emb_dim, rnn_hid_dim, num_layers=num_rnn_layers, bidirectional=False, dropout=dropout)
+        self.output = nn.Linear(rnn_hid_dim, len(trg_vocab) + 3)
+
+    def forward(self, src, previous_output, rnn_state):
+        src_embedded = self.src_embedding(src)
+        trg_embedded = self.trg_embedding(previous_output)
+        rnn_input = torch.cat((src_embedded, trg_embedded), dim=2)
+        rnn_output, rnn_state = self.rnn(rnn_input, rnn_state)
+        outputs = self.output(rnn_output)
+        return outputs, rnn_state
+
+
+class RQL1(nn.Module):
+    def __init__(self, src_vocab, trg_vocab, rnn_hid_dim,
+                 dropout, num_rnn_layers, linear_dim):
+
+        super().__init__()
+        src_emb_dim = src_vocab.vectors.size()[1]
+        trg_emb_dim = trg_vocab.vectors.size()[1]
+        self.dropout = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
+
+        self.src_embedding = nn.Embedding(len(src_vocab), src_emb_dim).from_pretrained(src_vocab.vectors, freeze=True)
+        self.trg_embedding = nn.Embedding(len(trg_vocab), trg_emb_dim).from_pretrained(trg_vocab.vectors, freeze=True)
+        self.rnn = nn.GRU(src_emb_dim + trg_emb_dim, rnn_hid_dim, num_layers=num_rnn_layers, bidirectional=False, dropout=dropout)
+        self.linear = nn.Linear(rnn_hid_dim, linear_dim)
+        self.output = nn.Linear(linear_dim, len(trg_vocab) + 3)
+
+    def forward(self, src, previous_output, rnn_state):
+        src_embedded = self.src_embedding(src)
+        trg_embedded = self.trg_embedding(previous_output)
+        rnn_input = torch.cat((src_embedded, trg_embedded), dim=2)
+        rnn_output, rnn_state = self.rnn(rnn_input, rnn_state)
+        linear_output = self.relu(self.linear(self.dropout(rnn_output)))
+        outputs = self.output(linear_output)
+        return outputs, rnn_state
+
+
+class RQL2(nn.Module):
+    def __init__(self, src_vocab, trg_vocab, rnn_hid_dim, dropout,
+                 num_rnn_layers, linear1_dim, linear2_dim):
+
+        super().__init__()
+        src_emb_dim = src_vocab.vectors.size()[1]
+        trg_emb_dim = trg_vocab.vectors.size()[1]
+        self.dropout = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
+
+        self.src_embedding = nn.Embedding(len(src_vocab), src_emb_dim).from_pretrained(src_vocab.vectors, freeze=True)
+        self.trg_embedding = nn.Embedding(len(trg_vocab), trg_emb_dim).from_pretrained(trg_vocab.vectors, freeze=True)
+        self.rnn = nn.GRU(src_emb_dim + trg_emb_dim, rnn_hid_dim, num_layers=num_rnn_layers, bidirectional=False, dropout=dropout)
+        self.linear1 = nn.Linear(rnn_hid_dim, linear1_dim)
+        self.linear2 = nn.Linear(linear1_dim, linear2_dim)
+        self.output = nn.Linear(linear2_dim, len(trg_vocab) + 3)
+
+    def forward(self, src, previous_output, rnn_state):
+        src_embedded = self.src_embedding(src)
+        trg_embedded = self.trg_embedding(previous_output)
+        rnn_input = torch.cat((src_embedded, trg_embedded), dim=2)
+        rnn_output, rnn_state = self.rnn(rnn_input, rnn_state)
+        linear1_output = self.relu(self.linear1(self.dropout(rnn_output)))
+        linear2_output = self.relu(self.linear2(self.dropout(linear1_output)))
+        outputs = self.output(linear2_output)
+        return outputs, rnn_state
