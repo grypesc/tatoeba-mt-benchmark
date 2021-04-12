61f7848495a4b192b3d4c495333fcd5f5c122741
grypesc <g.rypesc@wp.pl>
2021-04-12 21:28:59 +0200

    policy loss upgrade

--- criterions/rql_criterion.py
+++ criterions/rql_criterion.py
@@ -12,13 +12,13 @@ class RQLCriterion(nn.Module):
         self._mistranslation_loss_weight = 0
         self._policy_loss_weight = 0
         self.mistranslation_criterion = nn.CrossEntropyLoss(ignore_index=pad_index)
-        self.policy_criterion = nn.MSELoss()
+        self.policy_criterion = nn.MSELoss(reduction="sum")
 
     def forward(self, word_outputs, trg, Q_used, Q_target):
         _mistranslation_loss = self.mistranslation_criterion(word_outputs, trg)
 
         if self.training:
-            _policy_loss = self.policy_criterion(Q_used, Q_target)
+            _policy_loss = self.policy_criterion(Q_used, Q_target)/torch.count_nonzero(Q_target)
             self.ro_to_k *= self.RO
             w_k = (self.RO - self.ro_to_k) / (1 - self.ro_to_k)
             self._mistranslation_loss_weight = w_k * self._mistranslation_loss_weight + (1 - w_k) * float(_mistranslation_loss)
