f9b3be4e42539d8962c2f2ca84caed76bc92540b
grypesc <g.rypesc@wp.pl>
2021-04-17 23:37:05 +0200

    rql adaptive criterion

--- criterions/rql_criterion.py
+++ criterions/rql_criterion.py
@@ -30,6 +30,32 @@ class RQLCriterion(nn.Module):
             return None, _mistranslation_loss, None
 
 
+class RQLAdaptiveCriterion(nn.Module):
+
+    def __init__(self, ro, pad_index):
+        super().__init__()
+        self.RO = ro
+        self.ro_to_k = 1
+        self._mistranslation_loss_weight = 0
+        self._policy_loss_weight = 0
+        self.mistranslation_criterion = nn.CrossEntropyLoss(ignore_index=pad_index)
+        self.policy_criterion = nn.MSELoss(reduction="sum")
+
+    def forward(self, word_outputs, trg, Q_used, Q_target, mistranslation_loss_multiplier):
+        _mistranslation_loss = self.mistranslation_criterion(word_outputs, trg)
+
+        if self.training:
+            _policy_loss = self.policy_criterion(Q_used, Q_target)/torch.count_nonzero(Q_target)
+            self.ro_to_k *= self.RO
+            w_k = (self.RO - self.ro_to_k) / (1 - self.ro_to_k)
+            self._mistranslation_loss_weight = w_k * self._mistranslation_loss_weight + (1 - w_k) * float(_mistranslation_loss)
+            self._policy_loss_weight = w_k * self._policy_loss_weight + (1 - w_k) * float(_policy_loss)
+            loss = _policy_loss / self._policy_loss_weight + mistranslation_loss_multiplier * _mistranslation_loss / self._mistranslation_loss_weight
+            return loss, _mistranslation_loss, _policy_loss
+        else:
+            return None, _mistranslation_loss, None
+
+
 class RQLCriterionExp(nn.Module):
 
     def __init__(self, ro, pad_index, mistranslation_loss_multiplier):
