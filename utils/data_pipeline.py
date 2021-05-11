0fd56af0cb4820c9f2ca46970b4070f235b9cbd2
grypesc <g.rypesc@wp.pl>
2021-05-12 00:08:49 +0200

    logging vocabs lengths

--- utils/data_pipeline.py
+++ utils/data_pipeline.py
@@ -32,16 +32,18 @@ class DataPipeline:
         test_data = self.tensor_from_files(test_filepath)
         long_test_data = self.tensor_from_files(long_test_filepath)
 
-        print("Loaded {} {} training sentences".format(len(train_data), prefix))
-        print("Loaded {} validating sentences".format(len(val_data)))
-        print("Loaded {} test sentences".format(len(test_data)))
-        print("Loaded {} long-test sentences\n".format(len(long_test_data)))
-
         generate_batch = self.generate_null_batch if null_replaces_bos else self.generate_bos_batch
         self.train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, collate_fn=generate_batch, num_workers=0)
         self.valid_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False, collate_fn=generate_batch, num_workers=0)
         self.test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, collate_fn=generate_batch, num_workers=0)
         self.long_test_loader = DataLoader(long_test_data, batch_size=batch_size, shuffle=False, collate_fn=generate_batch, num_workers=0)
+
+        print("Loaded {} {} training sentences".format(len(train_data), prefix))
+        print("Loaded {} validating sentences".format(len(val_data)))
+        print("Loaded {} test sentences".format(len(test_data)))
+        print("Loaded {} long-test sentences".format(len(long_test_data)))
+        print("Source vocabulary has {} unique tokens".format(len(self.src_vocab)))
+        print("Target vocabulary has {} unique tokens\n".format(len(self.trg_vocab)))
 
     def build_vocabs(self, filepath):
         src_counter, trg_counter = Counter(), Counter()
