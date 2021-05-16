4486eb4891509e6557fca05b59272da477154fb4
grypesc <g.rypesc@wp.pl>
2021-05-17 00:48:38 +0200

    leaky resiproximator

--- models/rlst/approximators.py
+++ models/rlst/approximators.py
@@ -127,6 +127,52 @@ class ResidualApproximator(nn.Module):
         return input + rnn_output, rnn_new_state
 
 
+class LeakyResidualApproximator(nn.Module):
+
+    def __init__(self,
+                 src_vocab,
+                 trg_vocab,
+                 use_pretrained_embeddings,
+                 rnn_hid_dim,
+                 rnn_dropout,
+                 rnn_num_layers,
+                 src_embed_dim=256,
+                 trg_embed_dim=256,
+                 embedding_dropout=0.0):
+        super().__init__()
+
+        self.rnn_hid_dim = rnn_hid_dim
+        self.rnn_num_layers = rnn_num_layers
+        self.src_embedding = nn.Embedding(len(src_vocab), src_embed_dim)
+        self.trg_embedding = nn.Embedding(len(trg_vocab), trg_embed_dim)
+        self.embedding_dropout = nn.Dropout(embedding_dropout)
+        assert src_embed_dim + trg_embed_dim == rnn_hid_dim
+        self.rnns = nn.ModuleList(rnn_num_layers * [nn.GRU(rnn_hid_dim, rnn_hid_dim)])
+        self.linear = nn.Linear(rnn_hid_dim, rnn_hid_dim)
+        self.activation = nn.LeakyReLU()
+        self.output = nn.Linear(rnn_hid_dim, len(trg_vocab) + 3)
+
+    def forward(self, src, previous_output, rnn_states):
+        src_embedded = self.embedding_dropout(self.src_embedding(src))
+        trg_embedded = self.embedding_dropout(self.trg_embedding(previous_output))
+
+        rnn_input = torch.cat((src_embedded, trg_embedded), dim=2)
+        rnn_new_states = torch.zeros(rnn_states.size(), device=src_embedded.device)
+        res_out = None
+        for i, rnn in enumerate(self.rnns):
+            res_out, rnn_new_states[i, :] = self._skip_rep(rnn_input, rnn, rnn_states[i:i + 1])
+            rnn_input = res_out
+
+        leaky_output = self.activation(self.linear(res_out))
+        outputs = self.output(leaky_output)
+        return outputs, rnn_new_states
+
+    @staticmethod
+    def _skip_rep(input, rnn, rnn_state):
+        rnn_output, rnn_new_state = rnn(input, rnn_state)
+        return input + rnn_output, rnn_new_state
+
+
 class ResidualApproximator9000(nn.Module):
     """Bad boy with policy coming straight from GRU hidden states."""
 
